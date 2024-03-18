b52bc883d1f735ad6acb9c10a27a10b0c82ad10c
naz-az <nazzazlan93@gmail.com>
2024-03-18 10:21:47 +0800

    q

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-gunicorn -k eventlet -w 1 app:app --log-file -
+#!/bin/bash
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+gunicorn -k eventlet -w 1 -b 0.0.0.0:3000 app:app --log-file -
